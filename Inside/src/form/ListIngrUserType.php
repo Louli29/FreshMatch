832ffaf5ee3d6edeb44842ec02fb5ef279880781
Patebj <narimanehamiche4@gmail.com>
2025-02-20 18:44:42 +0100

    a verifier svp

---
+++ Inside/src/form/ListIngrUserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use App\Entity\ListIngrUtilisateur;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ListIngrUtilisateurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('ingredient', EntityType::class, [
+                'class' => Ingredient::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('utilisateur', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ListIngrUtilisateur::class,
+        ]);
+    }
+}
