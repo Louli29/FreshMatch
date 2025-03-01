dcd6e23027fa850b678bfe1e32c270e27fae72f6
atandabany <tandabany.adrien@hotmail.fr>
2025-03-02 00:41:00 +0100

    creation page inscription

--- Inside/src/form/UserType.php
+++ Inside/src/form/UserType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\form;
+namespace App\Form;
 
 use App\Entity\ListIngrUser;
 use App\Entity\User;
@@ -9,21 +9,46 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('name')
-            ->add('diet')
-            ->add('allergy')
-            ->add('listIngredient', EntityType::class, [
-                'class' => ListIngrUser::class,
-                'choice_label' => 'id',
-            ])
+           // ->add('email')
+           // ->add('roles')
+           // ->add('password')
+           // ->add('name')
+           // ->add('diet')
+           // ->add('allergy')
+           // ->add('listIngredient', EntityType::class, [
+           //     'class' => ListIngrUser::class,
+           //     'choice_label' => 'id',
+           // ])
+
+        ->add('name', TextType::class, [
+            'label' => 'Nom',
+            'attr' => ['class' => 'form-control']
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'E-mail',
+            'attr' => ['class' => 'form-control']
+        ])
+        ->add('password', PasswordType::class, [
+            'label' => 'Mot de passe',
+            'attr' => ['class' => 'form-control']
+        ])
+        ->add('diet', TextType::class, [
+            'label' => 'Votre régime alimentaire',
+            'attr' => ['class' => 'form-control']
+        ])
+        ->add('allergy', TextType::class, [
+            'label' => 'Vos allergies',
+            'attr' => ['class' => 'form-control']
+        ])
         ;
     }
 
