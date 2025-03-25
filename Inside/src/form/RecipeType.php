d74f63f51981e439943e5f1f822d7bd5fbd70049
atandabany <tandabany.adrien@hotmail.fr>
2025-03-25 22:45:07 +0100

    boostrap recipe

--- Inside/src/form/RecipeType.php
+++ Inside/src/form/RecipeType.php
@@ -4,12 +4,14 @@ namespace App\Form;
 
 use App\Entity\IngredientRecipe;
 use App\Entity\Recipe;
-use App\Entity\User;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
 class RecipeType extends AbstractType
 {
@@ -17,21 +19,52 @@ class RecipeType extends AbstractType
     {
         $builder
 
-            ->add('time')
-            ->add('name')
-            ->add('description')
-            ->add('step')
-            ->add('nbPerson')
-            ->add('allergys')
-            ->add('diet')
-            ->add('imageLink'
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la recette',
+                'attr' => ['class' => 'form-control'],
+            ])
 
-            )
+            ->add('allergys', TextType::class, [
+                'label' => 'Allergies',
+                'attr' => ['class' => 'form-control'],
+            ])
+
+            ->add('diet', TextType::class, [
+                'label' => 'Régime alimentaire',
+                'attr' => ['class' => 'form-control'],
+            ])
+
             ->add('ingredientRecipe', EntityType::class, [
                 'class' => IngredientRecipe::class,
                 'choice_label' => 'id',
                 'multiple' => true,
                 'required' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
+
+            ->add('step', TextareaType::class, [
+                'label' => 'Étapes',
+                'attr' => ['class' => 'form-control'],
+            ])
+
+            ->add('time', IntegerType::class, [
+                'label' => 'Temps (en minutes)',
+                'attr' => ['class' => 'form-control'],
+            ])
+
+            ->add('nbPerson', IntegerType::class, [
+                'label' => 'Nombre de personnes',
+                'attr' => ['class' => 'form-control'],
+            ])
+
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['class' => 'form-control'],
+            ])
+
+            ->add('imageLink', UrlType::class, [
+                'label' => 'Lien de l\'image',
+                'attr' => ['class' => 'form-control'],
             ])
         ;
     }
