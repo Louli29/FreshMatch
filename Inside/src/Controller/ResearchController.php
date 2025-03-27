f49382ba5b0671b800aacf31b99f07afe983dfd9
Cartier Lou <lou.cartier09@gmail.com>
2025-03-27 23:52:15 +0100

    saisie automatique

--- Inside/src/Controller/ResearchController.php
+++ Inside/src/Controller/ResearchController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Ingredient;
+use App\Repository\IngredientRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -22,5 +25,22 @@ final class ResearchController extends AbstractController
             'controller_name' => 'ResearchController',
             'ingredients' => $ingredients,
         ]);
+    }
+
+    #[Route('/autocomplete/ingredients', name: 'autocomplete_ingredients')]
+    public function autocomplete(Request $request, IngredientRepository $ingredientRepository): JsonResponse
+    {
+        $term = $request->query->get('term');
+
+        $results = $ingredientRepository->createQueryBuilder('i')
+            ->where('i.name LIKE :term')
+            ->setParameter('term', '%' . $term . '%')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult();
+
+        $ingredients = array_map(fn($ingredient) => ['name' => $ingredient->getName()], $results);
+
+        return new JsonResponse($ingredients);
     }
 }
