e849bfe3dde1e04636424541617f2aa4f267f2d4
Cartier Lou <lou.cartier09@gmail.com>
2025-03-27 20:06:03 +0100

    debut recherche

--- Inside/src/Controller/ResearchController.php
+++ Inside/src/Controller/ResearchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,12 +12,15 @@ final class ResearchController extends AbstractController
 
 {
     #[Route('/research', name: 'app_research')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $ingredients = $entityManager->getRepository(Ingredient::class)->findBy(array(), array('id' => 'DESC'), 10);
+
+
 
         return $this->render('research/index.html.twig', [
             'controller_name' => 'ResearchController',
-
+            'ingredients' => $ingredients,
         ]);
     }
 }
