4ac2cba417645433e652719b890de340c3f4196a
Steven200405 <huangsteven2004@gmail.com>
2025-03-28 09:33:38 +0100

    correct  bug

--- Inside/src/DataFixtures/UserFixtures.php
+++ Inside/src/DataFixtures/UserFixtures.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-#[AllowDynamicProperties] class UserFixtures extends Fixture
+#[AllowDynamicProperties] 
 class UserFixtures extends Fixture
 {
     private UserPasswordHasherInterface $passwordHasher;
