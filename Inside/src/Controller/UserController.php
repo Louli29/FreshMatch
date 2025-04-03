a2bb061f18d8a1fd4d75fea19f4845115d49ca23
atandabany <tandabany.adrien@hotmail.fr>
2025-04-03 19:37:40 +0200

    suite bootstrap v2

--- Inside/src/Controller/UserController.php
+++ Inside/src/Controller/UserController.php
@@ -120,7 +120,7 @@ final class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('home_page', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_account', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/edit.html.twig', [
